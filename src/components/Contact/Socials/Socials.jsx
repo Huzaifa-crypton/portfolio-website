342a8d469340c0829c1e6794a152eac6aba00bf8
Muhammad Huzaifa Khan ⚡ <huzaifa18072002@gmail.com>
2025-04-13 16:57:28 +0500

    Update resume

--- src/components/Contact/Socials/Socials.jsx
+++ src/components/Contact/Socials/Socials.jsx
@@ -97,7 +97,7 @@ const Socials = () => {
             {/* Resume */}
             <Grid item xs={12} style={{paddingLeft:0}} display={'flex'} justifyContent={"center"} alignItems={"center"}>
                 <a
-                    href={"https://drive.google.com/file/d/1uJOnd6izLJhPHiIJhZy3ShU04aEO09Mt/view"}
+                    href={"https://drive.google.com/file/d/1dx9UXQUbMEjr3OJntYvSmdhI4DZIzKWw/view"}
                     // download="Huzaifa_Khan_Resume.pdf"
                     target="_blank"
                     rel="noreferrer"
